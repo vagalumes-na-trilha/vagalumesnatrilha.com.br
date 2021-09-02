2e52673310ce9dc600a278d5015b2cb704fa173e
Daniel Rodrigo Fleck <danielfleck@gmail.com>
2021-09-02 12:04:01 -0300

    chore: instala o encore e o bootstrap

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];

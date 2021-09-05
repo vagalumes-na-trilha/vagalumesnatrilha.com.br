b206001b1469df2ab9e388a54ed88d5d686a1af5
Daniel Rodrigo Fleck <danielfleck@gmail.com>
2021-09-05 13:55:26 -0300

    feat: implementa desativar item do carrossel

--- src/Controller/Admin/CarrosselController.php
+++ src/Controller/Admin/CarrosselController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Repository\CarrosselRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,5 +19,21 @@ class CarrosselController extends AbstractController
         return $this->render('admin/carrossel/index.html.twig', [
             'carrossel' => $carrossel
         ]);
+    }
+
+    #[Route('/adm/carrossel/desativar', name: 'admin_carrossel_desativar')]
+    public function desativarCarrossel(CarrosselRepository $carrosselRepository, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $carrossel = $request->request->all('carrossel');
+
+        $carrossel = $carrosselRepository->find($carrossel['id']);
+
+        $carrossel->setDatahoraFim(new \DateTime('now'));
+
+        $entityManager->persist($carrossel);
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_carrossel');
     }
 }
